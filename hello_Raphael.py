71df79f106ccfb7456ec4c5a8dd30326cfe704c1
waphhh <75032460+waphhh@users.noreply.github.com>
2020-11-25 10:30:37 -0500

    Add files via upload

---
+++ hello_Raphael.py
@@ -0,0 +1,3 @@
+# Il s'agit d'un test formatif qui ne compte pas
+print("Je m'appelle Raphael Ghelani, mes passe-temps favoris sont de jouer aux jeux vidéo, de faire du sport et de passer du temps en famille")
+# J'ai maintenant terminé le test formatif
